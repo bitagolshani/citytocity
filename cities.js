d4dc4642384244dd445e4355d7c903971025c4fc
Bita Golshani <bg2et@virginia.edu>
2018-08-31 14:34:56 -0400

    Working time conversion!

--- cities.js
+++ cities.js
@@ -4,7 +4,7 @@
 /* called upon button click */
 function getLatLong() {
     var loc1 = document.getElementById("firstloc").value;
-   // console.log(loc1);
+    // console.log(loc1);
     var loc2 = document.getElementById("secondloc").value;
 
     /*FIRST CITY lat&lng*/
@@ -37,8 +37,8 @@ function getLatLong() {
         secondLat = request2.responseText.substring(secondLat, secondLat + 9);
         secondLong = request2.responseText.indexOf('"lng"') + 6; //lastIndexOf gives same result? 
         secondLong = request2.responseText.substring(secondLong, secondLong + 9);
-      //  console.log(secondLat);
-      //  console.log(secondLong);
+        //  console.log(secondLat);
+        //  console.log(secondLong);
     }
 
     /* GETS CURRENT TIME for both cities */
@@ -65,22 +65,53 @@ function getLatLong() {
 
         requestTime2.onload = function () {
             window.currTime2 = requestTime2.responseText.indexOf("formatted") + 23; //hr:min /* GLOBAL VARIABLE */ 
-            currTime2 = requestTime2.responseText.substring(currTime2, currTime2 + 5);
+            window.currTime2 = requestTime2.responseText.substring(currTime2, currTime2 + 5);
 
-            /* DISPLAY TIMES AND RESULTS */
-            currTimeHour = window.currTime.substring(0, 2);
-            currTime2Hour = window.currTime2.substring(0, 2);
-            if (currTime2Hour < 8 && currTime2Hour >= 0) {
-                var tryAgain = 8 - parseInt(currTime2Hour);
-                document.getElementById("results").innerHTML = "<span style='color:red;'> Now isn't a great time to call. </span> <br> Their current time, " + window.currTime2 + ", isn't within 08:00 to 24:00. Try again in " + tryAgain + " hours.";
-            }
-            else if (currTimeHour < 8 && currTimeHour > 0) {
-                var tryAgain = 8 - parseInt(currTimeHour);
-                document.getElementById("results").innerHTML = "<span style='color:red;'> Now isn't a great time to call. </span> <br> Your current time, " + window.currTime + ", isn't within 08:00 to 24:00. Try again in " + tryAgain + " hours.";
-            }
-            else
-                document.getElementById("results").innerHTML = "<span style='color:green;'> Now is a good time to call. </span> <br> It's currently " + window.currTime2 + " in their location.";
+            /* DISPLAY TIMES AND RESULTS- sends in current 12hr/24hr selection*/
+            printMsg(document.getElementById("timeFormat").value);
         }
-
     }
 }
+
+
+
+/* DISPLAY TIMES AND RESULTS */
+function printMsg(timeVal) {
+    if (typeof currTime !== 'undefined') { //if time designation is changed before time is inputted
+        currTimeHour = window.currTime.substring(0, 2);
+        currTime2Hour = window.currTime2.substring(0, 2);
+        let message;
+
+        if (currTime2Hour < 8 && currTime2Hour >= 0) {
+            var tryAgain = 8 - parseInt(currTime2Hour);
+            message = "<span style='color:red;'> Now isn't a great time to call. </span> <br> Their current time, *timeHere*, isn't within 08:00 to 24:00. Try again in " + tryAgain + " hours.";
+            timeResult(timeVal, currTime2, message, true);
+        }
+        else if (currTimeHour < 8 && currTimeHour > 0) {
+            var tryAgain = 8 - parseInt(currTimeHour);
+            message = "<span style='color:red;'> Now isn't a great time to call. </span> <br> Your current time, *timeHere*, isn't within 08:00 to 24:00. Try again in " + tryAgain + " hours.";
+            timeResult(timeVal, currTime, message, true);
+        }
+        else {
+            message = "<span style='color:green;'> Now is a good time to call. </span> <br> It's currently *timeHere* in their location.";
+            timeResult(timeVal, currTime2, message, false);
+        }
+    }
+}
+
+function timeResult(timeVal, time, message, boundaries) {
+    if (timeVal == 1) { //24hr time 
+        message = message.replace("*timeHere*", time);
+        document.getElementById("results").innerHTML = message;
+    }
+    else { //12hr time 
+        time = time.split(":");
+        var end = parseInt(time[0]) >= 12 ? "PM" : "AM";
+        time[0] = (parseInt(time[0]) + 11) % 12 + 1; //update hours 
+        message = message.replace("*timeHere*", time[0] + ":" + time[1] + " " + end);
+        if (boundaries) { message = message.replace("08:00 to 24:00", "8:00 AM to 12:00 AM"); }
+        document.getElementById("results").innerHTML = message;
+    }
+}
+
+//note: "var" keyword is function-scoped, not global; "let" is local 
